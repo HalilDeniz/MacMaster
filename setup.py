6ca41ae3ab8985c584bbd1a3aad6e32107f82261
halil ibrahim deniz <62566319+HalilDeniz@users.noreply.github.com>
2023-12-02 06:09:46 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MacMaster: MAC Address Changer',
+    version='1.0.0',
+    author='Halil İbrahim Deniz',
+    author_email='halildeniz313@gmail.com',
+    description='A versatile MAC Address Changer tool for network anonymity and testing.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/HalilDeniz/MacMaster',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'macmaster=macmaster.macmaster:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
